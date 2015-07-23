545b3485edba110fbf0f2b8fe3a28bd67e5d088d
phillipmadsen <pmadsen2013@gmail.com>
2015-07-23 15:57:24 -0600

    fixed dependencies
    full update and fix

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,14 +8,15 @@ $baseDir = dirname($vendorDir);
 return array(
     $vendorDir . '/symfony/var-dumper/Symfony/Component/VarDumper/Resources/functions/dump.php',
     $vendorDir . '/nikic/php-parser/lib/bootstrap.php',
-    $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
     $vendorDir . '/ircmaxell/password-compat/lib/password.php',
+    $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
     $vendorDir . '/psy/psysh/src/Psy/functions.php',
     $vendorDir . '/danielstjules/stringy/src/Create.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
     $vendorDir . '/guzzlehttp/psr7/src/functions.php',
     $vendorDir . '/illuminate/html/helpers.php',
-    $vendorDir . '/barryvdh/laravel-debugbar/src/helpers.php',
     $vendorDir . '/laracasts/flash/src/Laracasts/Flash/functions.php',
+    $vendorDir . '/barryvdh/laravel-debugbar/src/helpers.php',
+    $baseDir . '/app/Helper/helpers.php',
 );
